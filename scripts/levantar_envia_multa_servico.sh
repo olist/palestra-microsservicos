01c537bbe9341cc5828d55cb1203b8f8164696df
Douglas Silva <douglas_hss@hotmail.com.br>
2020-05-20 17:18:18 -0300

    Melhorando script que levanta o serviço

--- scripts/levantar_envia_multa_servico.sh
+++ scripts/levantar_envia_multa_servico.sh
@@ -9,7 +9,7 @@ aws --endpoint-url http://localhost:4100 sqs create-queue --queue-name envia_mul
 aws --endpoint-url http://localhost:4100 sns subscribe \
 --topic-arn arn:aws:sns:us-east-1:000000000000:multa__criada \
 --protocol sqs --notification-endpoint http://us-east-1.goaws.com:4100/100010001000/envia_multa_servico__multa__criada
-
+clear
 # Iniciando o serviço
 source .venv/bin/activate
 python -m envia_multa_servico
